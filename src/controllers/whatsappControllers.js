d94aefd9ebbb62a49a284e6f32663e9d375b095c
Oscar Aranda <oscararanda1984@gmail.com>
2024-03-24 20:21:09 -0300

    se agrego servicio whatsapp

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,8 +1,13 @@
+const { text } = require('express');
+const fs = require('fs');
+const myConsole = new console.Console(fs.createWriteStream('./logs.txt'));
+const whatsappService = require('../services/whatsapp.service')
+
 const VerifyToken = (req, res ) => {
     try {
-        var accessToken = 'RSDSDG42314tsDFSDV53';
-        var token = req.query('hub.verify_token');
-        var challenge = req.query('hub.challenge');
+        var accessToken = 'RSDSDG42314TSDFSDV53';
+        var token = req.query['hub.verify_token'];
+        var challenge = req.query['hub.challenge'];
         if(challenge != null && token != null && token == accessToken){
             res.send(challenge); 
         }else{
@@ -18,8 +23,60 @@ const VerifyToken = (req, res ) => {
 
 
 const RecibedMessage = ( req, res ) => {
-    console.info('Hola received')
-    res.send("Hola recibeMessage")
+   
+   try {
+    var entry = (req.body['entry'])[0];
+    var changes = (entry['changes'])[0];
+    var value = changes['value'];
+    var messageObjet = value['messages'];
+
+    if (typeof messageObjet != 'undefined') {
+        var messages = messageObjet[0];
+        var number = messages['from'];
+        var text = GetTextUser( messages );
+        myConsole.log(text);
+
+        whatsappService.SendMessageWhatsApp( 'El usuario dijo: ' + text, number );
+       
+    }
+    res.send('EVENT_RECEIVED');
+
+   } catch (e) {
+    
+       myConsole.log(e);
+       res.send("EVENT_RECEIVED")
+   }
+   
+   
+}
+
+function GetTextUser(messages){
+    var text = '';
+    var typeMessage = messages['type']
+
+    if(typeMessage == 'text'){
+        text = (messages['text'])['body']
+    }
+    else if(typeMessage == 'interactive'){
+        var interactiveObject = messages['interactive'];
+        var typeInteractive = interactiveObject['type'];
+        
+
+        if(typeInteractive == 'button_reply'){
+            text = (interactiveObject['button_reply'])['title'];
+            
+        }else if(typeInteractive == 'list_reply'){
+            text = (interactiveObject['list_reply'])['title'];
+
+        }else{
+            myConsole.log('Sin mensaje');
+        }
+
+
+    }else{
+        myConsole.log('Sin mensaje');
+    }
+    return text;
 }
 
 
---
+++ src/services/whatsapp.service.js
@@ -0,0 +1,41 @@
+const https = require('https');
+const env = require('dotenv');
+
+function SendMessageWhatsApp(textResponse, number) {
+    const data = JSON.stringfy(
+        {
+            "messaging_product": "whatsapp",
+            "to": number,
+            "type": "text",
+            "text": {
+                "body": textResponse
+            }
+        });
+
+    const options = { 
+        host: 'graph.facebook.com',
+        path: '/v18.0/266547939874298/messages',
+        method: 'POST',
+        body: data,
+        header: {
+            'content-Type': 'application/json',
+            authorization: process.env.AUTHORIZATION
+
+        }
+    };
+    const req = https.request(options, res => {
+        res.on('data', d=>{
+            process.stdout.write(d)
+        });
+    }) ;
+    req.on('error', error => {
+        console.error( error );
+    });
+
+    req.write(data);
+    req.end();
+
+}
+module.exports = {
+    SendMessageWhatsApp
+}
